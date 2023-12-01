4b2d600b08729074aba1d341e58f328ea1379552
biruytskovsky <david.biruykov.02@mail.ru>
2023-12-02 00:36:17 +0200

    Добавьте файлы проекта.

---
+++ zadacha 4 2/zadacha 4 2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "write number";
+    cin >> N;
+
+    switch (N) {
+    case 1:
+        cout << " Milka" << endl;
+        break;
+    case 2:
+        cout << "Nescafe" << endl;
+        break;
+    case 3:
+        cout << "Malboro" << endl;
+        break;
+    case 4:
+        cout << "Coka Kola" << endl;
+        break;
+    case 5:
+        cout << "Durex" << endl;
+        break;
+    default:
+        cout << "Not found, try again" << endl;
+    }
+
+    return 0;
+}
